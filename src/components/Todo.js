4cb3a5050fc7967b154934a6607193a2e33cee35
Abhay <abhaysharma.developer@gmail.com>
2023-05-26 22:48:38 +0530

    Feature added - Completed button functional

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 export default function Todo(props) {
-  const deleteHandler = () => {
-    props.setListItem( props.listItem.filter((element)=> element.id !== props.task.id))
+ 
+    const deleteHandler = () => {
+        props.setListItem( props.listItem.filter((element)=> element.id !== props.task.id))
+    };
+
+  const completeHandler = () => {
+    props.setListItem(props.listItem.map((element)=>{
+        if(element.id === props.task.id){
+            return { ...element , completed : !element.completed}
+        }
+        return element ;
+    } ))
     
   };
 
   return (
     <div className="todo">
-      <li className="todo-item">{props.text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item  ${props.task.completed ? 'completed':""}`}>{props.text}</li>
+      <button className='complete-btn' onClick={completeHandler}>
         <i className="fas fa-check"></i>
       </button>
       <button className="trash-btn" onClick={deleteHandler}>
